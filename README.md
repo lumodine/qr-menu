164b504abfe0590055324ede331f84a37179f78d
enteresanlikk <zeki1198@gmail.com>
2024-10-11 15:30:34 +0300

    Rename project name.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# qr-menu-ui
+# ui
--- src/data/settings.ts
+++ src/data/settings.ts
@@ -1,6 +1,6 @@
 import type { Settings } from "@/types"
 
 export const settings: Settings = {
-    name: "QR Menü",
+    name: "QR Menü - LumoDine",
     logo: "https://placehold.co/100x55/png"
 }
