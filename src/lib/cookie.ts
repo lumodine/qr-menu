1a674806822e325356150b4a5ebcb1140a9ccf54
enteresanlikk <zeki1198@gmail.com>
2024-11-20 01:52:27 +0300

    Fix cookie bug.

--- src/lib/cookie.ts
+++ src/lib/cookie.ts
@@ -5,8 +5,8 @@ import { cookies } from "next/headers"
 const get = async (key: string) => (await cookies()).get(key)?.value
 
 const set = async (key: string, value: string, options: any = {}) => {
-  const cookieExpire = new Date()
-  cookieExpire.setFullYear(cookieExpire.getFullYear() + 1)
+  const cookieExpire = new Date();
+  cookieExpire.setFullYear(cookieExpire.getFullYear() + 1);
 
   (await cookies()).set(
     key,
