adaae43ad70b896e461f7735ff046c7b3e778de1
enteresanlikk <zeki1198@gmail.com>
2024-11-28 17:16:50 +0300

    Update category hero.

--- src/components/app/category.tsx
+++ src/components/app/category.tsx
@@ -14,35 +14,37 @@ export const CategoryHero = ({ tenant, category }: CategoryHeroProps) => {
             style={{ '--bg-image': `url(${category.image})` }}
             className="bg-[image:var(--bg-image)] bg-center bg-no-repeat bg-cover overflow-hidden bg-primary/50"
         >
-            <div className="flex flex-col gap-2 items-center justify-center w-full h-[30vh] lg:h-[40vh] bg-black/50">
-                {
-                    tenant.logo && (
-                        <Link href={"/"}>
-                            <Image
-                                src={tenant.logo}
-                                alt={tenant.name}
-                                width={100}
-                                height={100}
-                                loading={"lazy"}
-                                className="rounded-full w-[100px] h-[100px] md:w-[200px] md:h-[200px]"
-                            />
-                        </Link>
-                    )
-                }
-                {
-                    category.translations[0].name && (
-                        <h1 className="text-2xl md:text-3xl lg:text-6xl font-bold text-white drop-shadow-2xl">
-                            {category.translations[0].name}
-                        </h1>
-                    )
-                }
-                {
-                    category.translations[0].description && (
-                        <p className="text-sm md:text-md lg:text-lg text-white drop-shadow-2xl">
-                            {category.translations[0].description}
-                        </p>
-                    )
-                }
+            <div className="bg-black/50">
+                <div className="container flex flex-col gap-4 items-center justify-center w-full h-[30vh] lg:h-[40vh] ">
+                    {
+                        tenant.logo && (
+                            <Link href={"/"}>
+                                <Image
+                                    src={tenant.logo}
+                                    alt={tenant.name}
+                                    width={100}
+                                    height={100}
+                                    loading={"lazy"}
+                                    className="rounded-full w-[100px] h-[100px] md:w-[200px] md:h-[200px]"
+                                />
+                            </Link>
+                        )
+                    }
+                    {
+                        category.translations[0].name && (
+                            <h1 className="text-2xl md:text-3xl lg:text-6xl font-bold text-white drop-shadow-2xl">
+                                {category.translations[0].name}
+                            </h1>
+                        )
+                    }
+                    {
+                        category.translations[0].description && (
+                            <p className="text-sm md:text-md lg:text-lg text-white drop-shadow-2xl">
+                                {category.translations[0].description}
+                            </p>
+                        )
+                    }
+                </div>
             </div>
         </section>
     )
