4f8ebe5b051162b6cf2a846905a103e7db5234a7
enteresanlikk <zeki1198@gmail.com>
2024-12-20 17:06:44 +0300

    Add tags page.

---
+++ src/app/[tenantAlias]/tags/[itemId]/page.tsx
@@ -0,0 +1,40 @@
+import {notFound} from "next/navigation";
+import qrMenuService from "@/services/qr-menu.service";
+import {BackToMenuButton} from "@/components/common/back-to-menu-button";
+import {ProductList} from "@/components/product/product-list";
+import {TagHero} from "@/components/tag/tag-hero";
+
+type CategoryPageProps = {
+  params: Promise<{
+    tenantAlias: string;
+    itemId: string;
+  }>;
+};
+
+export default async function CategoryPage({params}: CategoryPageProps) {
+  const {tenantAlias, itemId} = await params;
+
+  const itemResponse = await qrMenuService.getItemById(tenantAlias, itemId);
+
+  if (!itemResponse.success || !itemResponse.data) {
+    return notFound();
+  }
+
+  const {data: items} = await qrMenuService.getSubItems(tenantAlias, itemId);
+
+  const item = itemResponse.data;
+
+  return (
+    <>
+      <TagHero tag={item} />
+
+      <section className="container">
+        <div className="py-2">
+          <BackToMenuButton />
+        </div>
+      </section>
+
+      <ProductList products={items} />
+    </>
+  );
+}
---
+++ src/components/tag/tag-hero.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import type {Tag} from "@/types";
+import Link from "next/link";
+import Image from "next/image";
+import {Tag as TagIcon} from "lucide-react";
+import {useAppContext} from "@/contexts/AppContext";
+import {cn} from "@/utils/shadcn";
+
+export type TagHeroProps = {
+  tag: Tag;
+};
+
+export const TagHero = ({tag}: TagHeroProps) => {
+  const {tenant, language, defaultLanguage} = useAppContext();
+
+  let translation = tag.translations.find(
+    (translation) => translation.language._id === language.language._id,
+  );
+
+  if (!translation) {
+    translation = tag.translations.find(
+      (translation) => translation.language._id === defaultLanguage.language._id,
+    );
+  }
+
+  return (
+    <section
+      className={cn(
+        tag.theme?.color && `theme-${tag.theme?.color}`,
+        "bg-center bg-no-repeat bg-cover overflow-hidden bg-primary/50",
+      )}
+    >
+      <div className="bg-black/50 text-center">
+        <div className="container flex flex-col gap-4 items-center justify-center w-full h-[30vh] lg:h-[40vh] ">
+          {tenant.logo && (
+            <Link href={"/"}>
+              <Image
+                alt={tenant.name}
+                className="rounded-sm w-[100px] h-[100px] md:w-[200px] md:h-[200px]"
+                height={100}
+                loading={"lazy"}
+                src={tenant.logo}
+                width={100}
+              />
+            </Link>
+          )}
+          {translation?.name && (
+            <h1 className="text-2xl md:text-3xl lg:text-6xl font-bold text-white drop-shadow-2xl">
+              <TagIcon className="inline-block w-6 h-6 md:w-8 md:h-8 lg:w-12 lg:h-12" />{" "}
+              {translation?.name}
+            </h1>
+          )}
+          {translation?.description && (
+            <p className="text-sm md:text-md lg:text-lg text-white drop-shadow-2xl">
+              {translation?.description}
+            </p>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+TagHero.displayName = "TagHero";
