192c26e4c112b78a653ba243c0424e0d38c1440f
enteresanlikk <zeki1198@gmail.com>
2024-12-25 00:09:56 +0300

    Fix amount digit format.

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -2,5 +2,6 @@ export function formatAmount(locale: string, amount: number) {
   return new Intl.NumberFormat(locale, {
     style: "decimal",
     minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(amount);
 }
